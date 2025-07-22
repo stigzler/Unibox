87c491983e983a198d0cbfd9faf064b51071dabb
Steve <stigzler@msn.com>
2025-07-22 15:56:50 +0100

    Add project files.

---
+++ Unibox/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Unibox
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ThemeToggle_Click(object sender, RoutedEventArgs e)
+        {
+            if (DarkModeMI.IsChecked == true)
+            {
+                // Change to Dark Theme
+            }
+            else
+            {
+                // Change to Light Theme
+            }
+        }
+
+  
+    }
+}
